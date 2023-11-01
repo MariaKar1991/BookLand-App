f1d40517556aa00d6dd43aaccbcd8cf88e6c8e88
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 13:16:48 +0200

    Update app.2.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,26 @@
+// Loading environment variables from a .env file
 require("dotenv").config();
+
+// Importing required modules
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
 const cors = require("cors");
 
+// Creating an instance of the express application
 const app = express();
 
 // Middlewares
+// Parsing incoming JSON requests
 app.use(express.json());
+
+// Enabling Cross-Origin Resource Sharing (CORS)
 app.use(cors());
+
+// Using the book routes for URLs starting with '/books'
 app.use("/books", router); // localhost:5000/books
 
+// Connecting to MongoDB using the provided URL in the environment variables
 mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
@@ -18,6 +28,9 @@ mongoose
   })
   .then(() => console.log("Connected To Database"))
   .then(() => {
-    app.listen(5000);
+    // Starting the server on port 5000 after successful database connection
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
   })
   .catch((err) => console.log(err));
