0548d19e66ae648eff7fb67d89d1148d084c445a
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 13:28:58 +0200

    Update App.2.js

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,3 +1,4 @@
+// Importing necessary modules and components from React and React Router
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
@@ -7,18 +8,32 @@ import Books from "./components/Book/Books";
 import About from "./components/About";
 import BookDetail from "./components/Book/BookDetail";
 
+// Main component for the application structure
 function App() {
+  // Rendering the application layout with header and routes
   return (
     <React.Fragment>
+      {/* Header component for the application */}
       <header>
         <Header />
       </header>
+
+      {/* Main content area with React Router routes */}
       <main>
         <Routes>
+          {/* Route for the home page */}
           <Route path="/" element={<Home />} exact />
+          
+          {/* Route for adding a new book */}
           <Route path="/add" element={<AddBook />} exact />
+          
+          {/* Route for displaying the list of books */}
           <Route path="/books" element={<Books />} exact />
+          
+          {/* Route for displaying information about the application */}
           <Route path="/about" element={<About />} exact />
+          
+          {/* Route for displaying details of a specific book */}
           <Route path="/books/:id" element={<BookDetail />} exact />
         </Routes>
       </main>
@@ -26,4 +41,6 @@ function App() {
   );
 }
 
+// Exporting the App component for use in other parts of the application
 export default App;
+
