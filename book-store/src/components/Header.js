7c752bb427f7e6bfa9f14a308f999949924905d4
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 13:26:19 +0200

    Update Header.2.js

--- book-store/src/components/Header.js
+++ book-store/src/components/Header.js
@@ -1,20 +1,28 @@
+// Importing necessary modules and components from Material-UI, React, and MUI icons
 import React from "react";
 import { AppBar, Typography, Toolbar, Tabs, Tab } from "@mui/material";
 import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
+// Functional component for the application header
 const Header = () => {
+  // State for managing the active tab
   const [value, setValue] = useState();
+
+  // Rendering the application header with navigation tabs
   return (
     <div>
       <AppBar sx={{ backgroundColor: "#232F9D" }} position="sticky">
         <Toolbar>
+          {/* Home icon with a link to the home page */}
           <NavLink to="/" style={{ color: "white" }}>
             <Typography>
               <LibraryBooksOutlinedIcon />
             </Typography>
           </NavLink>
+
+          {/* Navigation tabs for different sections of the application */}
           <Tabs
             sx={{ ml: "auto" }}
             textColor="inherit"
@@ -22,8 +30,13 @@ const Header = () => {
             value={value}
             onChange={(e, val) => setValue(val)}
           >
+            {/* Tab for adding a new book */}
             <Tab LinkComponent={NavLink} to="/add" label="Add product" />
+            
+            {/* Tab for displaying the list of books */}
             <Tab LinkComponent={NavLink} to="/books" label="Books" />
+            
+            {/* Tab for displaying information about the application */}
             <Tab LinkComponent={NavLink} to="/about" label="About Us" />
           </Tabs>
         </Toolbar>
@@ -32,4 +45,5 @@ const Header = () => {
   );
 };
 
+// Exporting the Header component for use in other parts of the application
 export default Header;
