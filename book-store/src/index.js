8b231cca2dd704beaaa9315948e1ad4700b37b80
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 13:32:28 +0200

    Update index.2.js

--- book-store/src/index.js
+++ book-store/src/index.js
@@ -1,12 +1,17 @@
+// Importing necessary modules and components from React and ReactDOM
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// Creating a root for concurrent rendering using ReactDOM.createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Rendering the application within the root
 root.render(
   <React.StrictMode>
+    {/* Wrapping the App component with BrowserRouter for routing */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
